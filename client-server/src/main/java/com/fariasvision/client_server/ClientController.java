00f3ae72679cecfddf1dcc3759fca6fdf1a19323
caiofariaas <caiofarias077@gmail.com>
2024-07-22 14:00:58 -0300

    :sparkles: feat: Resource Server

--- client-server/src/main/java/com/fariasvision/client_server/ClientController.java
+++ client-server/src/main/java/com/fariasvision/client_server/ClientController.java
@@ -6,10 +6,19 @@ import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2Aut
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 @RestController
 public class ClientController {
+
+    WebClient webClient;
+
+    public ClientController(WebClient.Builder builder){
+        this.webClient = builder
+                .baseUrl("http://localhost:9090")
+                .build();
+    }
 
     @GetMapping("/home")
     public Mono<String> home(@RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient client, @AuthenticationPrincipal OidcUser oidcUser){
@@ -23,5 +32,17 @@ public class ClientController {
                         client.getRefreshToken().getTokenValue(),
                         oidcUser.getIdToken().getTokenValue(),
                         oidcUser.getClaims()));
+    }
+
+    @GetMapping("/tasks")
+    public Mono<String> getTasks(@RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient client){
+        return webClient
+                .get()
+                .uri("tasks")
+                .header("Authorization", "Bearer %s".formatted(client
+                                                                                        .getAccessToken()
+                                                                                        .getTokenValue()))
+                .retrieve()
+                .bodyToMono(String.class);
     }
 }
