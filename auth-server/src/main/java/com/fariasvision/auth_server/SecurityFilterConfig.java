68f55cdd1078b714effa766c0de750fab9c7cb7b
caiofariaas <caiofarias077@gmail.com>
2024-07-22 10:05:56 -0300

    first commit✨

---
+++ auth-server/src/main/java/com/fariasvision/auth_server/SecurityFilterConfig.java
@@ -0,0 +1,30 @@
+package com.fariasvision.auth_server;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+
+@Configuration
+public class SecurityFilterConfig {
+
+    @Bean
+    SecurityFilterChain authServerSecurityFilterChain(HttpSecurity http) throws Exception {
+        OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
+
+        http.getConfigurer(OAuth2AuthorizationServerConfigurer.class).oidc(
+                withDefaults());
+
+        http.exceptionHandling(e -> e.authenticationEntryPoint(
+                new LoginUrlAuthenticationEntryPoint("/login")
+        ))
+                .oauth2ResourceServer(conf -> conf.jwt(withDefaults()));
+
+        return http.build();
+    }
+}
