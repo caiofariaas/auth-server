d4a789273847b5a053d58459fa75f914c691ebf5
caiofariaas <caiofarias077@gmail.com>
2024-07-22 11:09:33 -0300

    :sparkles:feat: Client Server

---
+++ auth-server/src/main/java/com/fariasvision/auth_server/AuthorizationServerConfig.java
@@ -0,0 +1,14 @@
+package com.fariasvision.auth_server;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
+
+@Configuration
+public class AuthorizationServerConfig {
+
+    @Bean
+    AuthorizationServerSettings authorizationServerSettings() {
+        return AuthorizationServerSettings.builder().build();
+    }
+}
